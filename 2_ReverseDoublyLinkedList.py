9930e53b243f12eaf7f3950415be2486052c213d
Rohit Kumar <Rohit Kumar>
2018-09-14 02:59:05 +0530

    Started writing code for reversing a doubly linked list

---
+++ 2_ReverseDoublyLinkedList.py
@@ -0,0 +1,11 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data;
+        self.prev = self.Next = None
+
+
+
+class LinkedList(object):
+    def __rev_list(self):
+        # this method reverses a doubly linked list
+    
